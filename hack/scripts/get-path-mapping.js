5a9067a8b7097d51c0516983347e72f83be7b326
Enda Phelan <ephelan@redhat.com>
2021-05-17 17:11:43 +0100

    ci: use api version channels

---
+++ hack/scripts/get-path-mapping.js
@@ -0,0 +1,17 @@
+const apiPathMappings = require('../../.config/apiMappings.json');
+
+const args = process.argv.slice(2);
+
+// get file name argument
+if (!args || !args.length) {
+  throw new Error("GitHub Repository argument is required");
+}
+
+const githubRepo = args[0];
+
+const apiPath = apiPathMappings[githubRepo];
+if (!apiPath) {
+  throw new Error(`Unknown GitHub repo "${githubRepo}"`)
+}
+
+console.log(apiPath);
