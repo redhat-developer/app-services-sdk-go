5709320ffef8cc8f60d13530b42f8e5f6d7a9a98
Enda Phelan <ephelan@redhat.com>
2021-05-17 17:11:43 +0100

    ci: use api version channels

---
+++ hack/scripts/get-raw-openapi-url.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const args = process.argv.slice(2);
+const [githubRepository, branch, openApiDirectory, openApiFileName] = args;
+
+
+const rawOpenApiURL = path.join("https://raw.githubusercontent.com", githubRepository,
+  branch || 'main',
+  openApiDirectory,
+  openApiFileName
+);
+
+console.log(rawOpenApiURL);
