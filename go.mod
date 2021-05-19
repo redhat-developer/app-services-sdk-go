c773f33b25ec1dfb589d02fbf387eba363f89da1
Enda Phelan <ephelan@redhat.com>
2021-05-19 16:47:46 +0100

    chore: generate API mock

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/redhat-developer/app-services-sdk-go
 
 go 1.16
+
+require golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
--- hack/scripts/generate.sh
+++ hack/scripts/generate.sh
@@ -19,4 +19,13 @@ fi
 npx npx @openapitools/openapi-generator-cli version-manager set 5.1.1
 npx @openapitools/openapi-generator-cli generate -g go -i "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" --package-name "${PACKAGE_NAME}" --git-user-id="redhat-developer" --git-repo-id="app-services-sdk-go/$OUTPUT_PATH" -p "generateInterfaces=true" --ignore-file-override=.openapi-generator-ignore
 
+go mod download
+go mod tidy
+
+# generate API interface mock
+mock_api_file="$OUTPUT_PATH/default_api_mock.go"
+
+rm -rf $mock_api_file
+moq -out "$mock_api_file" "$OUTPUT_PATH" DefaultApi
+
 rm -rf "$OPENAPI_FILENAME"
