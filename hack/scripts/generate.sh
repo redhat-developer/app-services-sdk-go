6073c05032d89cb7509fb736f6041f32160ac95b
Enda <ephelan@redhat.com>
2021-05-20 10:14:49 +0100

    chore: restructure packages (#34)

--- hack/scripts/generate.sh
+++ hack/scripts/generate.sh
@@ -3,7 +3,7 @@
 # set output path of the API client
 API_CLIENT_KEY="$GITHUB_REPO/$OPENAPI_FILENAME"
 API_GROUP="$(node $(dirname $0)/get-mapped-config.js "$API_CLIENT_KEY" "apiGroup")"
-API_VERSION="$(node $(dirname $0)/get-mapped-config.js "$API_CLIENT_KEY" "apiVersion")"
+API_VERSION="api$(node $(dirname $0)/get-mapped-config.js "$API_CLIENT_KEY" "apiVersion")"
 # set the Go package name
 PACKAGE_NAME="$(node $(dirname $0)/get-mapped-config.js "$API_CLIENT_KEY" "packageName")"
 OUTPUT_PATH="$API_GROUP/$PACKAGE_NAME/$API_VERSION"
@@ -16,7 +16,7 @@ if [ $? != 0 ]; then
   exit $?
 fi
 
-npx npx @openapitools/openapi-generator-cli version-manager set 5.1.1
+npx @openapitools/openapi-generator-cli version-manager set 5.1.1
 npx @openapitools/openapi-generator-cli generate -g go -i "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" --package-name "${PACKAGE_NAME}" --git-user-id="redhat-developer" --git-repo-id="app-services-sdk-go/$OUTPUT_PATH" -p "generateInterfaces=true" --ignore-file-override=.openapi-generator-ignore
 
 go mod download
---
+++ kafka/go.mod
@@ -0,0 +1,5 @@
+module github.com/redhat-developer/app-services-sdk-go/apis/kafka
+
+go 1.16
+
+require golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
