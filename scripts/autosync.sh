e05cf69a800300ae613d39ab3ad9ed2ac9075d2e
Wojciech Trocki <wtrocki@redhat.com>
2021-05-21 15:29:34 +0100

    fix: remove autosync. It is not needed in source repository

--- scripts/autosync.sh
+++
@@ -1,7 +0,0 @@
-## List of the folders common across different SDK's 
-## This file targets to provide list of the directories that are common across all SDKS
-## Note you are in source repo. This script is not intended to be used here
-source=../app-services-sdk-go
-folders="${source}/.chglog ${source}/.config ${source}/.github ${source}/scripts"
-
-cp -r ${folders} ./
