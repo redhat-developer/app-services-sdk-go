8cbad287a679498f944b2ce8bfcf656e696ad202
Enda <ephelan@redhat.com>
2021-05-25 14:27:30 +0100

    chore(scripts): improved the metadata structure for API clients (#51)

--- scripts/fetch_api.sh
+++ scripts/fetch_api.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # get the download URL of the OpenAPI spec file
-OPENAPI_FILE_URL=$(node $(dirname $0)/get-raw-openapi-url.js "$GITHUB_REPO" "$BRANCH" "$OPENAPI_DIRECTORY" "$OPENAPI_FILENAME")
+OPENAPI_FILE_URL=$(node $(dirname $0)/get-raw-openapi-url.js "$CLIENT_PAYLOAD")
+OPENAPI_FILE_NAME=$(node $(dirname $0)/get-openapi-filename.js "$CLIENT_PAYLOAD")
 
 # download the OpenAPI file
-curl -H "Authorization: token $BF2_TOKEN" "$OPENAPI_FILE_URL" -o "$OPENAPI_FILENAME"
+curl -H "Authorization: token $BF2_TOKEN" "$OPENAPI_FILE_URL" -o "$OPENAPI_FILE_NAME"
 if [ $? != 0 ]; then
   exit $?
 fi
