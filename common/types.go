811ff7e603b02ed5a953feacea1e6d0c5604b8e5
Enda Phelan <ephelan@redhat.com>
2021-05-26 13:02:22 +0100

    feat: add API client wrapper

---
+++ common/types.go
@@ -0,0 +1,17 @@
+package common
+
+import (
+	"net/http"
+	"net/url"
+)
+
+// APIConfig defines the available configuration options
+// to customise the API client settings
+type APIConfig struct {
+	// HTTPClient is a custom HTTP client
+	HTTPClient *http.Client
+	// Debug enables debug-level logging
+	Debug      bool
+	// ServerURL sets a custom API server base URL
+	ServerURL  *url.URL
+}
---
+++ kafkamgmt/api_client.go
@@ -0,0 +1,41 @@
+package kafkamgmt
+
+import (
+	"github.com/redhat-developer/app-services-sdk-go/common"
+
+	apiv1 "github.com/redhat-developer/app-services-sdk-go/kafkamgmt/apiv1"
+)
+
+// APIConfig defines the available configuration options
+// to customise the API client settings
+type APIConfig = common.APIConfig
+
+// NewAPIClient returns a new KafkaManagement v1 API client
+// using a custom config
+func NewAPIClient(cfg *APIConfig) apiv1.DefaultApi {
+	apiCfg := apiv1.NewConfiguration()
+	if cfg == nil {
+		client := apiv1.NewAPIClient(apiCfg)
+		return client.DefaultApi
+	}
+
+	if cfg.HTTPClient == nil {
+		apiCfg.HTTPClient = cfg.HTTPClient
+	}
+	if cfg.ServerURL != nil {
+		apiCfg.Host = cfg.ServerURL.Host
+		apiCfg.Scheme = cfg.ServerURL.Scheme
+	}
+
+	apiCfg.Debug = cfg.Debug
+
+	client := apiv1.NewAPIClient(apiCfg)
+
+	return client.DefaultApi
+}
+
+// NewAPIClientWithDefaults returns a new KafkaManagement v1 API client
+// using the default configuration values
+func NewAPIClientWithDefaults(config *APIConfig) apiv1.DefaultApi {
+	return NewAPIClient(nil)
+}
