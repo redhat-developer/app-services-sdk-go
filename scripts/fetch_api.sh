db7846210e50fa1086a58ddc08b0e35231839543
Enda Phelan <ephelan@redhat.com>
2021-05-26 16:39:16 +0100

    ci: more changes

--- scripts/fetch_api.sh
+++ scripts/fetch_api.sh
@@ -6,6 +6,7 @@ OPENAPI_FILE_NAME=$(node $(dirname $0)/get-openapi-filename.js "$CLIENT_PAYLOAD"
 
 # download the OpenAPI file
 curl -H "Authorization: token $BF2_TOKEN" "$OPENAPI_FILE_URL" -o "$OPENAPI_FILE_NAME"
+
 if [ $? != 0 ]; then
   exit $?
 fi
