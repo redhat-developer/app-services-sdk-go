5094736cae2fc0d9a312124d873d23978ce58e1d
Enda Phelan <ephelan@redhat.com>
2021-06-02 12:16:04 +0100

    chore: use individual arguments in generate scripts

--- scripts/get-openapi-filename.js
+++ scripts/get-openapi-filename.js
@@ -3,8 +3,5 @@
 const path = require('path');
 
 const args = process.argv.slice(2);
-const clientPayload = JSON.parse(args[0]);
 
-const downloadUrl = clientPayload.download_url;
-
-console.log(path.basename(downloadUrl));
+console.log(path.basename(args[0]));
