9c4614eb20a5f60754bfb25832f6ee34adb7949c
Enda <ephelan@redhat.com>
2021-06-16 12:34:03 +0100

    ci: generate API client on merge to main (#108)

--- internal/apigen/generator/golang/generate.go
+++ internal/apigen/generator/golang/generate.go
@@ -5,10 +5,10 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 
+	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/redhat-developer/app-services-sdk-go/internal/apigen/generator/common"
-
-	"github.com/redhat-developer/app-services-sdk-go/internal/apigen/openapi"
 )
 
 type GoGen struct {
@@ -16,18 +16,15 @@ type GoGen struct {
 }
 
 // Generate generates an API client in Golang
-func (g *GoGen) Generate() error {
-	packageName := fmt.Sprintf("%vclient", g.Config.ClientMetadata.APIGroup)
-	apiVersion := fmt.Sprintf("api%v", g.Config.ClientMetadata.APIVersion)
-	outputPath := fmt.Sprintf("%v/%v/client", g.Config.ClientMetadata.APIGroup, apiVersion)
-	inputFile, err := openapi.GetFileName(g.Config.Input)
-	if err != nil {
+func (g *GoGen) Generate() (err error) {
+	inputFile := g.Config.Input
+	if _, err = os.Stat(inputFile); err != nil {
 		return err
 	}
-	_, err = os.Stat(inputFile)
-	if err != nil {
-		return err
-	}
+
+	clientMetadata := g.Config.ClientMetadata
+	packageName := clientMetadata.PackageName()
+	outputPath := clientMetadata.OutputPath()
 
 	cmdArgs := []string{
 		common.CmdName,
@@ -45,38 +42,67 @@ func (g *GoGen) Generate() error {
 		cmdArgs = append(cmdArgs, "-t", g.Config.TemplatesDir)
 	}
 
-	err = exec.Command("npx", cmdArgs...).Run()
+	out, err := exec.Command("npx", cmdArgs...).CombinedOutput()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, string(out))
+		return err
+	} else {
+		fmt.Println(string(out))
+	}
+
+	doc, err := openapi3.NewLoader().LoadFromFile(inputFile)
 	if err != nil {
 		return err
 	}
 
-	err = generateMocks(outputPath)
-	if err != nil {
-		return err
+	if len(doc.Tags) == 0 {
+		_ = generateMocks("default", outputPath)
+	}
+	for _, tag := range doc.Tags {
+		_ = generateMocks(tag.Name, outputPath)
 	}
 
-	// when all is done move the OpenAPI file to .openapi
-	return os.Rename(inputFile, path.Join(".openapi", inputFile))
+	return nil
 }
 
-func generateMocks(output string) error {
+// generate mocks for each API interface present
+func generateMocks(tag string, output string) error {
 	err := os.Setenv("GO111MODULE", "on")
 	if err != nil {
 		return err
 	}
-	err = exec.Command("go", "get", "github.com/matryer/moq").Run()
+	out, err := exec.Command("go", "get", "-u", "github.com/matryer/moq").CombinedOutput()
 	if err != nil {
+		fmt.Fprintln(os.Stderr, string(out))
 		return err
+	} else {
+		fmt.Println(string(out))
 	}
-	defaultMockApiFile := path.Join(output, "default_api_mock.go")
+	mockFilePath := path.Join(output, fmt.Sprintf("%v_api_mock.go", tag))
 
-	err = exec.Command("rm", "-f", defaultMockApiFile).Run()
+	out, err = exec.Command("rm", "-f", mockFilePath).CombinedOutput()
 	if err != nil {
+		fmt.Fprintln(os.Stderr, string(out))
 		return err
+	} else {
+		fmt.Println(string(out))
 	}
-	err = exec.Command("moq", "-out", defaultMockApiFile, output, "DefaultApi").Run()
+	interfaceName := fmt.Sprintf("%vApi", strings.Title(strings.ToLower(tag)))
+
+	out, err = exec.Command("moq", "-out", mockFilePath, output, interfaceName).CombinedOutput()
 	if err != nil {
+		fmt.Fprintln(os.Stderr, string(out))
 		return err
+	} else {
+		fmt.Println(string(out))
 	}
-	return exec.Command("go", "mod", "tidy").Run()
+	out, err = exec.Command("go", "mod", "tidy").CombinedOutput()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, string(out))
+		return err
+	} else {
+		fmt.Println(string(out))
+	}
+
+	return nil
 }
--- internal/apigen/go.mod
+++ internal/apigen/go.mod
@@ -3,6 +3,7 @@ module github.com/redhat-developer/app-services-sdk-go/internal/apigen
 go 1.16
 
 require (
+	github.com/getkin/kin-openapi v0.63.0
 	github.com/google/go-github/v35 v35.3.0
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
 )
