2742e71b25d7fec7fbb9fc93961d8568b55e219d
Red Hat Application Services CI <84090353+app-services-ci@users.noreply.github.com>
2021-06-24 11:43:34 -0400

    chore(all): re-generate OpenAPI client(s) (#159)
    Co-authored-by: app-services-ci <app-services-ci@users.noreply.github.com>

--- registrymgmt/apiv1/client/client.go
+++ registrymgmt/apiv1/client/client.go
@@ -3,7 +3,7 @@
  *
  * Managed Service Registry cloud.redhat.com API Management API that lets you create new registry instances. Registry is a datastore for standard event schemas and API designs. Service Registry enables developers to manage and share the structure of their data using a REST interface. For example, client applications can dynamically push or pull the latest updates to or from the registry without needing to redeploy. Registry is an Managed version of upstream project called Apicurio Registry. Apicurio Registry also enables developers to create rules that govern how registry content can evolve over time. For example, this includes rules for content validation and version compatibility.
  *
- * API version: 0.0.1
+ * API version: 0.0.4
  * Contact: rhosak-eval-support@redhat.com
  */
 
@@ -41,7 +41,7 @@ var (
 	xmlCheck  = regexp.MustCompile(`(?i:(?:application|text)/xml)`)
 )
 
-// APIClient manages communication with the Service Registry Fleet Manager API v0.0.1
+// APIClient manages communication with the Service Registry Fleet Manager API v0.0.4
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
