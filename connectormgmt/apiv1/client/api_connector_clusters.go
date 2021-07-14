4e2c46ea936b23062ae51d6b9cdaecfac03cd978
app-services-ci <app-services-ci@users.noreply.github.com>
2021-07-14 19:21:49 +0000

    chore(all): re-generate OpenAPI client(s)

--- connectormgmt/apiv1/client/api_connector_clusters.go
+++ connectormgmt/apiv1/client/api_connector_clusters.go
@@ -93,6 +93,19 @@ type ConnectorClustersApi interface {
 	 * @return ConnectorClusterList
 	 */
 	ListConnectorClustersExecute(r ApiListConnectorClustersRequest) (ConnectorClusterList, *_nethttp.Response, error)
+
+	/*
+	 * UpdateConnectorClusterById udpate a connector cluster
+	 * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	 * @param connectorClusterId The id of the connector cluster
+	 * @return ApiUpdateConnectorClusterByIdRequest
+	 */
+	UpdateConnectorClusterById(ctx _context.Context, connectorClusterId string) ApiUpdateConnectorClusterByIdRequest
+
+	/*
+	 * UpdateConnectorClusterByIdExecute executes the request
+	 */
+	UpdateConnectorClusterByIdExecute(r ApiUpdateConnectorClusterByIdRequest) (*_nethttp.Response, error)
 }
 
 // ConnectorClustersApiService ConnectorClustersApi service
@@ -793,4 +806,137 @@ func (a *ConnectorClustersApiService) ListConnectorClustersExecute(r ApiListConn
 	}
 
 	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+type ApiUpdateConnectorClusterByIdRequest struct {
+	ctx _context.Context
+	ApiService ConnectorClustersApi
+	connectorClusterId string
+	connectorCluster *ConnectorCluster
+}
+
+func (r ApiUpdateConnectorClusterByIdRequest) ConnectorCluster(connectorCluster ConnectorCluster) ApiUpdateConnectorClusterByIdRequest {
+	r.connectorCluster = &connectorCluster
+	return r
+}
+
+func (r ApiUpdateConnectorClusterByIdRequest) Execute() (*_nethttp.Response, error) {
+	return r.ApiService.UpdateConnectorClusterByIdExecute(r)
+}
+
+/*
+ * UpdateConnectorClusterById udpate a connector cluster
+ * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ * @param connectorClusterId The id of the connector cluster
+ * @return ApiUpdateConnectorClusterByIdRequest
+ */
+func (a *ConnectorClustersApiService) UpdateConnectorClusterById(ctx _context.Context, connectorClusterId string) ApiUpdateConnectorClusterByIdRequest {
+	return ApiUpdateConnectorClusterByIdRequest{
+		ApiService: a,
+		ctx: ctx,
+		connectorClusterId: connectorClusterId,
+	}
+}
+
+/*
+ * Execute executes the request
+ */
+func (a *ConnectorClustersApiService) UpdateConnectorClusterByIdExecute(r ApiUpdateConnectorClusterByIdRequest) (*_nethttp.Response, error) {
+	var (
+		localVarHTTPMethod   = _nethttp.MethodPut
+		localVarPostBody     interface{}
+		localVarFormFileName string
+		localVarFileName     string
+		localVarFileBytes    []byte
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "ConnectorClustersApiService.UpdateConnectorClusterById")
+	if err != nil {
+		return nil, GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/api/connector_mgmt/v1/kafka_connector_clusters/{connector_cluster_id}"
+	localVarPath = strings.Replace(localVarPath, "{"+"connector_cluster_id"+"}", _neturl.PathEscape(parameterToString(r.connectorClusterId, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := _neturl.Values{}
+	localVarFormParams := _neturl.Values{}
+	if r.connectorCluster == nil {
+		return nil, reportError("connectorCluster is required and must be specified")
+	}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{"application/json"}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	// body params
+	localVarPostBody = r.connectorCluster
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFormFileName, localVarFileName, localVarFileBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarHTTPResponse, err
+	}
+
+	localVarBody, err := _ioutil.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = _ioutil.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 {
+			var v Error
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 404 {
+			var v Error
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 500 {
+			var v Error
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+		}
+		return localVarHTTPResponse, newErr
+	}
+
+	return localVarHTTPResponse, nil
 }
