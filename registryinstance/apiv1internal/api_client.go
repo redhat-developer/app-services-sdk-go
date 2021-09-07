4baabafbe747b24edf83c724be14da96682e84b1
Wojciech Trocki <wtrocki@redhat.com>
2021-09-07 11:04:14 +0100

    fix: remove unused import

--- registryinstance/apiv1internal/api_client.go
+++ registryinstance/apiv1internal/api_client.go
@@ -2,7 +2,6 @@ package registryinstance
 
 import (
 	"github.com/redhat-developer/app-services-sdk-go/core"
-	"net/http"
 
 	apiv1 "github.com/redhat-developer/app-services-sdk-go/registryinstance/apiv1internal/client"
 )
