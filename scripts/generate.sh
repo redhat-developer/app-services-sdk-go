06018cc2e29ea6fcd1bf7f3b93b71aeccc9817b0
Wojciech Trocki <wtrocki@redhat.com>
2022-09-23 11:47:07 +0100

    fix: remove generate changes

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -26,45 +26,45 @@ npx @openapitools/openapi-generator-cli version-manager set 5.2.0
 echo "Generating SDKs"
 additional_properties="generateInterfaces=true,enumClassPrefix=true"
 
-# OPENAPI_FILENAME=".openapi/kas-fleet-manager.yaml"
-# PACKAGE_NAME="kafkamgmtclient"
-# OUTPUT_PATH="kafkamgmt/apiv1/client"
+OPENAPI_FILENAME=".openapi/kas-fleet-manager.yaml"
+PACKAGE_NAME="kafkamgmtclient"
+OUTPUT_PATH="kafkamgmt/apiv1/client"
 
-# generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
-# OPENAPI_FILENAME=".openapi/srs-fleet-manager.json"
-# PACKAGE_NAME="registrymgmtclient"
-# OUTPUT_PATH="registrymgmt/apiv1/client"
+OPENAPI_FILENAME=".openapi/srs-fleet-manager.json"
+PACKAGE_NAME="registrymgmtclient"
+OUTPUT_PATH="registrymgmt/apiv1/client"
 
-# generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
-# OPENAPI_FILENAME=".openapi/connector_mgmt.yaml"
-# PACKAGE_NAME="connectormgmtclient"
-# OUTPUT_PATH="connectormgmt/apiv1/client"
+OPENAPI_FILENAME=".openapi/connector_mgmt.yaml"
+PACKAGE_NAME="connectormgmtclient"
+OUTPUT_PATH="connectormgmt/apiv1/client"
 
-# generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
-# OPENAPI_FILENAME=".openapi/kafka-admin-rest.yaml"
-# PACKAGE_NAME="kafkainstanceclient"
-# OUTPUT_PATH="kafkainstance/apiv1/client"
+OPENAPI_FILENAME=".openapi/kafka-admin-rest.yaml"
+PACKAGE_NAME="kafkainstanceclient"
+OUTPUT_PATH="kafkainstance/apiv1/client"
 
-# generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
-# OPENAPI_FILENAME=".openapi/ams.json"
-# PATCH_FILE=".openapi/ams.patch" 
-# PACKAGE_NAME="accountmgmtclient"
-# OUTPUT_PATH="accountmgmt/apiv1/client"
+OPENAPI_FILENAME=".openapi/ams.json"
+PATCH_FILE=".openapi/ams.patch" 
+PACKAGE_NAME="accountmgmtclient"
+OUTPUT_PATH="accountmgmt/apiv1/client"
 
-# patch $OPENAPI_FILENAME < $PATCH_FILE
+patch $OPENAPI_FILENAME < $PATCH_FILE
 
-# rm -Rf $OUTPUT_PATH
-# npx @openapitools/openapi-generator-cli generate -g go -i \
-#     "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" \
-#     --package-name="${PACKAGE_NAME}" \
-#     --additional-properties=$additional_properties \
-#     --ignore-file-override=./accountmgmt/.openapi-generator-ignore
+rm -Rf $OUTPUT_PATH
+npx @openapitools/openapi-generator-cli generate -g go -i \
+    "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" \
+    --package-name="${PACKAGE_NAME}" \
+    --additional-properties=$additional_properties \
+    --ignore-file-override=./accountmgmt/.openapi-generator-ignore
 
-# git checkout -- $OPENAPI_FILENAME
+git checkout -- $OPENAPI_FILENAME
 
 echo "generating registry instance SDK "
 
@@ -94,14 +94,14 @@ OUTPUT_PATH="registryinstance/apiv1internal/client"
 generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
 
-# OPENAPI_FILENAME=".openapi/service-accounts.yaml"
-# PACKAGE_NAME="serviceaccountsclient"
-# OUTPUT_PATH="serviceaccountmgmt/apiv1/client"
+OPENAPI_FILENAME=".openapi/service-accounts.yaml"
+PACKAGE_NAME="serviceaccountsclient"
+OUTPUT_PATH="serviceaccountmgmt/apiv1/client"
 
-# generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
-# OPENAPI_FILENAME=".openapi/smartevents_mgmt.yaml"
-# PACKAGE_NAME="smarteventsmgmtclient"
-# OUTPUT_PATH="smarteventsmgmt/apiv1alpha/client"
+OPENAPI_FILENAME=".openapi/smartevents_mgmt.yaml"
+PACKAGE_NAME="smarteventsmgmtclient"
+OUTPUT_PATH="smarteventsmgmt/apiv1alpha/client"
 
-# generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
