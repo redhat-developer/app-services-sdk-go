d99ca959415922cd3c4fa750c627fdb1e1dd8ac9
app-services-ci <app-services-ci@users.noreply.github.com>
2022-11-02 17:39:58 +0000

    chore(all): re-generate OpenAPI client(s)

--- smarteventsmgmt/apiv1alpha/client/api_processors.go
+++ smarteventsmgmt/apiv1alpha/client/api_processors.go
@@ -220,6 +220,16 @@ func (a *ProcessorsApiService) ProcessorsAPIAddProcessorToBridgeExecute(r ApiPro
 			newErr.model = v
 			return localVarReturnValue, localVarHTTPResponse, newErr
 		}
+		if localVarHTTPResponse.StatusCode == 402 {
+			var v ErrorsList
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
 		if localVarHTTPResponse.StatusCode == 404 {
 			var v ErrorsList
 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
