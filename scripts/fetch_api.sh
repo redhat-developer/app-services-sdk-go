b58dd363e1aab205decd3c75a35be5a26d1ee458
Jakub Senko <m@jsenko.net>
2023-01-13 15:05:04 +0100

    ci: setup automated schema updates for registryinstance/v1

--- scripts/fetch_api.sh
+++ scripts/fetch_api.sh
@@ -2,12 +2,17 @@
 
 # get the download URL of the OpenAPI spec file
 OPENAPI_FILE_URL=$1
-OPENAPI_FILE_NAME=$(dirname $0)/../.openapi/$(basename $OPENAPI_FILE_URL)
+OPENAPI_FILE_NAME=$(basename $OPENAPI_FILE_URL)
+
+# Special case when downloading Service Registry Instance schema file,
+# to keep the original (manually updated) file name.
+if [ $OPENAPI_FILE_NAME == "openapi.json" ]; then
+  OPENAPI_FILE_NAME="registry-instance.json"
+fi
+OPENAPI_FILE_TARGET=$(dirname $0)/../.openapi/$OPENAPI_FILE_NAME
 
 # download the OpenAPI file
-curl "$OPENAPI_FILE_URL" --output $OPENAPI_FILE_NAME
+curl "$OPENAPI_FILE_URL" --output $OPENAPI_FILE_TARGET
 if [ $? != 0 ]; then
   exit $?
 fi
-
-
