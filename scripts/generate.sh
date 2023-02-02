6f9a8b0b331209c926536334c5035e5b32889502
Jakub Senko <m@jsenko.net>
2023-02-02 14:18:03 +0100

    fix: update registry instance schema preprocessing

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -69,18 +69,9 @@ git checkout -- $OPENAPI_FILENAME
 echo "generating registry instance SDK "
 
 cd .openapi
-echo "Removing codegen "
-cat registry-instance.json | jq 'del(.paths."x-codegen-contextRoot")' > registry-instance-tmp.json
-mv -f registry-instance-tmp.json registry-instance.json
-
-echo "Ensuring only single tag is created "
-cat registry-instance.json | jq 'walk( if type == "object" and has("tags") 
-       then .tags |= select(.[0])
-       else . end )' > registry-instance-tmp.json
-mv -f registry-instance-tmp.json registry-instance.json
 
 echo "Removing invalid datetime definitions"
-sed -i '' 's/date-time/utc-date/' registry-instance.json
+sed -i 's/date-time/utc-date/' registry-instance.json
 
 cd ..
 
